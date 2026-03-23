bcd6be267cd683633f4c541d4a01dd47a6b9b3fa
João Reis <j.victor.reis20044@gmail.com>
2026-03-23 19:55:44 -0300

    Aula 01 Hello World

---
+++ src/introduction/Aula01HelloWorld.java
@@ -0,0 +1,11 @@
+package introduction;
+// Comentário de uma linha
+/*
+    Comentário de
+    múltiplas linhas
+ */
+public class Aula01HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
